55e13c786f026169493ff9dd999136f1782efc12
Eduardo Heise <eduferreiraj@gmail.com>
2023-08-31 13:13:30 -0300

    typing and documenting
    Signed-off-by: Eduardo Heise <eduferreiraj@gmail.com>

--- documents.py
+++ documents.py
@@ -3,10 +3,22 @@ from PyPDF2 import PdfReader
 
 
 class Documents:
-    def __init__(self, path='documents/'):
+    def __init__(self, path='documents/') -> None:
+        """
+        Initialize the Documents class.
+
+        Args:
+            path (str): The path to the directory containing PDF documents.
+        """
         self.path = path
 
-    def gather_documents(self):
+    def gather_documents(self) -> str:
+        """
+        Gather text content from all PDF documents in the specified directory.
+
+        Returns:
+            str: Preprocessed text content from all PDF documents.
+        """
         text = ''
         for file in glob(f'{self.path}/*pdf'):
             doc = PdfReader(file)
@@ -14,11 +26,20 @@ class Documents:
                 text += page.extract_text()
         return self.preprocess_documents(text)
 
-    def preprocess_documents(self, text):
-        # Remove breaklines
+    def preprocess_documents(self, text) -> str:
+        """
+        Preprocess the extracted text from documents.
+
+        Args:
+            text (str): The text content extracted from documents.
+
+        Returns:
+            str: Preprocessed text with breaklines removed and non-ASCII characters removed.
+        """
+        # Removing breaklines
         text = text.replace('\n', ' ')
 
-        # Remove non-ASCII characters
+        # Removing non-ASCII characters
         text = ''.join(char for char in text if ord(char) < 128)
         return text
 
